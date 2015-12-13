ee52d65dcada1ead9b0c5f8caa19efbf3b7a9046
Satish <mukkojusatish@gmail.com>
2015-12-14 00:23:12 +0530

    change database

--- library/Model.php
+++ library/Model.php
@@ -4,7 +4,8 @@ class Model {
 
     function __construct() {
         if (ISLIVE == true) {
-            $this->db = new PDO('mysql:host=mysql.cdsg3fcsjios.ap-southeast-1.rds.amazonaws.com:3306;dbname=dambo_database_apps', 'root', 'dambo');
+//            $this->db = new PDO('mysql:host=mysql.cdsg3fcsjios.ap-southeast-1.rds.amazonaws.com:3306;dbname=dambo_database_apps', 'root', 'dambo');
+              $this->db = new PDO('mysql:host=localhost;dbname=dambo_database_apps', 'root', 'dambo');
         } else {
             $this->db = new PDO('mysql:host=localhost;dbname=dambo_database_apps', 'root', 'dambo');
         }
